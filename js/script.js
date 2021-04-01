7ebebdae10b85cd3ef2d80bb1462699476503ba9
Robert Love <robertjohnlove@gmail.com>
2021-04-01 15:59:42 +1100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,41 +1,60 @@
-var weight = document.getElementById('weight');
-var unit = document.getElementById('unit');
-var weightValue;
-var unitValue;
-var coefficient = 0.45359237;
-var weightPounds;
-var weightKilograms;
-var volumeExpert;
-var volumeAdvanced;
-var volumeIntermediate;
-var volumeBeginner;
+// The weight and unit form controls
+weight = document.getElementById('weight');
+unit = document.getElementById('unit');
+
+// The coefficient for converting lbs to kgs and vice versa
+coefficient = 0.45359237;
+
+// Rules define the difference in volume from the expert medium volume
+rules = {
+  expert: {
+    low: -1.666666666666668,
+    medium: 0,
+    high: 0.333333333333332
+  },
+  advanced: {
+    low: 1,
+    medium: 1.5,
+    high: 2
+  },
+  intermediate: {
+    low: 3,
+    medium: 3.5,
+    high: 4
+  },
+  beginner: {
+    low: 5,
+    medium: 6,
+    high: 7
+  }
+};
 
 function update() {
 
   weightValue = parseFloat(weight.value);
   unitValue = unit.value;
 
-  console.log(weightValue, unitValue);
-
   if (unitValue == 'lb') {
     weightPounds = weightValue;
     weightKilograms = weightPounds * coefficient;
-  }
-
-  if (unitValue == 'kg') {
+  } else {
     weightKilograms = weightValue;
     weightPounds = weightKilograms / coefficient;
   }
 
-  volumeExpert = weightPounds / 6;
+  document.getElementById('weight-pounds').textContent = weightPounds.toFixed(2);
+  document.getElementById('weight-kilograms').textContent = weightKilograms.toFixed(2);
 
-  weightPounds = weightPounds.toFixed(2);
-  weightKilograms = weightKilograms.toFixed(2);
-  volumeExpert = volumeExpert.toFixed(2);
+  volume = weightPounds / 6;
 
-  document.getElementById('weight-pounds').textContent = weightPounds;
-  document.getElementById('weight-kilograms').textContent = weightKilograms;
-  document.getElementById('volume-expert').textContent = volumeExpert;
+  for (level in rules) {
+    for (range in rules[level]) {
+      id = `${level}-${range}`;
+      levelVolume = volume + rules[level][range];
+      document.getElementById(id).textContent = levelVolume.toFixed(2);
+    }
+  }
+
 }
 
 weight.addEventListener('change', (event) => {
